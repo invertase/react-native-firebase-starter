7a9df8e9e454355e1acc38e7d7f25c7e755602df
Salakar <mike.diarmid@gmail.com>
2017-10-09 03:19:09 +0100

    ༼ つ ◕_◕ ༽つ GIVE STARTER APP

---
+++ App.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { StyleSheet, Platform, Image, Text, View } from 'react-native';
+
+import firebase from 'react-native-firebase';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      // firebase things?
+    };
+  }
+
+  componentDidMount() {
+    // firebase things?
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={require('./assets/RNFirebase512x512.png')} style={[styles.logo]} />
+        <Text style={styles.welcome}>
+          Welcome to the React Native{'\n'}Firebase starter project!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        {Platform.OS === 'ios' ? (
+          <Text style={styles.instructions}>
+            Press Cmd+R to reload,{'\n'}
+            Cmd+D or shake for dev menu
+          </Text>
+        ) : (
+          <Text style={styles.instructions}>
+            Double tap R on your keyboard to reload,{'\n'}
+            Cmd+M or shake for dev menu
+          </Text>
+        )}
+        <View style={styles.modules}>
+          <Text style={styles.modulesHeader}>The following Firebase modules are enabled:</Text>
+          {firebase.admob.nativeModuleExists && <Text style={styles.module}>Admob</Text>}
+          {firebase.analytics.nativeModuleExists && <Text style={styles.module}>Analytics</Text>}
+          {firebase.auth.nativeModuleExists && <Text style={styles.module}>Authentication</Text>}
+          {firebase.crash.nativeModuleExists && <Text style={styles.module}>Crash Reporting</Text>}
+          {firebase.firestore.nativeModuleExists && <Text style={styles.module}>Cloud Firestore</Text>}
+          {firebase.messaging.nativeModuleExists && <Text style={styles.module}>Messaging</Text>}
+          {firebase.perf.nativeModuleExists && <Text style={styles.module}>Performance Monitoring</Text>}
+          {firebase.database.nativeModuleExists && <Text style={styles.module}>Realtime Database</Text>}
+          {firebase.config.nativeModuleExists && <Text style={styles.module}>Remote Config</Text>}
+          {firebase.storage.nativeModuleExists && <Text style={styles.module}>Storage</Text>}
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  logo: {
+    height: 80,
+    marginBottom: 16,
+    width: 80,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  modules: {
+    margin: 20,
+  },
+  modulesHeader: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  module: {
+    fontSize: 14,
+    marginTop: 4,
+    textAlign: 'center',
+  }
+});
---
+++ android/app/src/main/java/com/rnfirebasestarter/MainApplication.java
@@ -0,0 +1,65 @@
+package com.rnfirebasestarter;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import io.invertase.firebase.RNFirebasePackage;
+// optional packages - add/remove as appropriate
+import io.invertase.firebase.admob.RNFirebaseAdMobPackage; //Firebase AdMob
+import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage; // Firebase Analytics
+import io.invertase.firebase.auth.RNFirebaseAuthPackage; // Firebase Auth
+import io.invertase.firebase.config.RNFirebaseRemoteConfigPackage; // Firebase Remote Config
+import io.invertase.firebase.crash.RNFirebaseCrashPackage; // Firebase Crash Reporting
+import io.invertase.firebase.database.RNFirebaseDatabasePackage; // Firebase Realtime Database
+import io.invertase.firebase.firestore.RNFirebaseFirestorePackage; // Firebase Firestore
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage; // Firebase Cloud Messaging
+import io.invertase.firebase.perf.RNFirebasePerformancePackage; // Firebase Performance
+import io.invertase.firebase.storage.RNFirebaseStoragePackage; // Firebase Storage
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+        new MainReactPackage(),
+        new RNFirebasePackage(),
+        // add/remove these packages as appropriate
+        new RNFirebaseAdMobPackage(),
+        new RNFirebaseAnalyticsPackage(),
+        new RNFirebaseAuthPackage(),
+        new RNFirebaseRemoteConfigPackage(),
+        new RNFirebaseCrashPackage(),
+        new RNFirebaseDatabasePackage(),
+        new RNFirebaseFirestorePackage(),
+        new RNFirebaseMessagingPackage(),
+        new RNFirebasePerformancePackage(),
+        new RNFirebaseStoragePackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
