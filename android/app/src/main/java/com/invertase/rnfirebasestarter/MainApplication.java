8eff2cc69a4a5de4db959af49a1da1ec6f758642
Salakar <mike.diarmid@gmail.com>
2018-07-13 13:16:57 +0100

    [android] add missing rnfirebase packages

--- android/app/src/main/java/com/invertase/rnfirebasestarter/MainApplication.java
+++ android/app/src/main/java/com/invertase/rnfirebasestarter/MainApplication.java
@@ -10,19 +10,22 @@ import com.facebook.soloader.SoLoader;
 
 import io.invertase.firebase.RNFirebasePackage;
 // optional packages - add/remove as appropriate
-import io.invertase.firebase.admob.RNFirebaseAdMobPackage; //Firebase AdMob
-import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage; // Firebase Analytics
-import io.invertase.firebase.auth.RNFirebaseAuthPackage; // Firebase Auth
-import io.invertase.firebase.config.RNFirebaseRemoteConfigPackage; // Firebase Remote Config
-import io.invertase.firebase.database.RNFirebaseDatabasePackage; // Firebase Realtime Database
-import io.invertase.firebase.firestore.RNFirebaseFirestorePackage; // Firebase Firestore
-import io.invertase.firebase.instanceid.RNFirebaseInstanceIdPackage; // Firebase Instance ID
-import io.invertase.firebase.links.RNFirebaseLinksPackage; // Firebase Dynamic Links
-import io.invertase.firebase.messaging.RNFirebaseMessagingPackage; // Firebase Cloud Messaging
-import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage; // Firebase Notifications
-import io.invertase.firebase.perf.RNFirebasePerformancePackage; // Firebase Performance
-import io.invertase.firebase.storage.RNFirebaseStoragePackage; // Firebase Storage
-import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage; // Crashlytics
+import io.invertase.firebase.admob.RNFirebaseAdMobPackage;
+import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.config.RNFirebaseRemoteConfigPackage;
+import io.invertase.firebase.crash.RNFirebaseCrashPackage;
+import io.invertase.firebase.database.RNFirebaseDatabasePackage;
+import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
+import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
+import io.invertase.firebase.functions.RNFirebaseFunctionsPackage;
+import io.invertase.firebase.instanceid.RNFirebaseInstanceIdPackage;
+import io.invertase.firebase.invites.RNFirebaseInvitesPackage;
+import io.invertase.firebase.links.RNFirebaseLinksPackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
+import io.invertase.firebase.perf.RNFirebasePerformancePackage;
+import io.invertase.firebase.storage.RNFirebaseStoragePackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -37,26 +40,29 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
+      return Arrays.asList(
         new MainReactPackage(),
         new RNFirebasePackage(),
         // add/remove these packages as appropriate
         new RNFirebaseAdMobPackage(),
         new RNFirebaseAnalyticsPackage(),
         new RNFirebaseAuthPackage(),
+        new RNFirebaseRemoteConfigPackage(),
+        new RNFirebaseCrashPackage(),
         new RNFirebaseCrashlyticsPackage(),
         new RNFirebaseDatabasePackage(),
         new RNFirebaseFirestorePackage(),
+        new RNFirebaseFunctionsPackage(),
         new RNFirebaseInstanceIdPackage(),
+        new RNFirebaseInvitesPackage(),
         new RNFirebaseLinksPackage(),
         new RNFirebaseMessagingPackage(),
         new RNFirebaseNotificationsPackage(),
         new RNFirebasePerformancePackage(),
-        new RNFirebaseRemoteConfigPackage(),
         new RNFirebaseStoragePackage()
       );
     }
-    
+
     @Override
     protected String getJSMainModuleName() {
       return "index";
