398d9d61b57cf5574a30d1aa7f4aaa335e0db512
Filipe Doutel Silva <filipe.doutelsilva@hotmail.fr>
2019-04-15 15:08:18 +0200

    Added firebase import inside app delegate (#108)
    * Added firebase import inside app delegate
    
    * Update AppDelegate.m

--- ios/RNFirebaseStarter/AppDelegate.m
+++ ios/RNFirebaseStarter/AppDelegate.m
@@ -10,11 +10,13 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <Firebase.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"RNFirebaseStarter"
