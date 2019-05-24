a7751c90c1f16e09f3b81bcf89323c636c8ca32e
Mike Hardy <github@mikehardy.net>
2019-05-24 11:26:26 -0500

    fix: correctly support API<21 with proper MultiDex support (#110)
    * fix: correctly support API<21 with proper MultiDex support
    
    Also, old Android APIs can't do network forwards, so dev bundles don't work,
    you have to pre-bundle the react-native app and include it even on dev builds.
    So I added an android-bundle npm script to run before starting the app on old devices
    
    * docs: clean up proguard shrink wording around multidex support

--- android/app/src/main/java/com/invertase/rnfirebasestarter/MainApplication.java
+++ android/app/src/main/java/com/invertase/rnfirebasestarter/MainApplication.java
@@ -1,6 +1,6 @@
 package com.invertase.rnfirebasestarter;
 
-import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
@@ -29,7 +29,7 @@ import io.invertase.firebase.storage.RNFirebaseStoragePackage;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends MultiDexApplication implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
