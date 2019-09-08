74836a07ee4b8ea1ba410ff523ab64dce3acd194
Andrew Schwartz <andrew@mk-dir.com>
2019-09-08 16:04:41 -0400

    update all pacages and react native version

--- android/app/src/main/java/com/invertase/rnfirebasestarter/MainApplication.java
+++ android/app/src/main/java/com/invertase/rnfirebasestarter/MainApplication.java
@@ -1,14 +1,18 @@
 package com.invertase.rnfirebasestarter;
 
-import android.support.multidex.MultiDexApplication;
+import androidx.multidex.MultiDexApplication;
+
+import android.util.Log;
+import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import io.invertase.firebase.RNFirebasePackage;
+//import io.invertase.firebase.RNFirebasePackage;
 // optional packages - add/remove as appropriate
 import io.invertase.firebase.admob.RNFirebaseAdMobPackage;
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
@@ -25,7 +29,6 @@ import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import io.invertase.firebase.perf.RNFirebasePerformancePackage;
 import io.invertase.firebase.storage.RNFirebaseStoragePackage;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends MultiDexApplication implements ReactApplication {
@@ -38,25 +41,25 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-        new MainReactPackage(),
-        new RNFirebasePackage(),
-        // add/remove these packages as appropriate
-        new RNFirebaseAdMobPackage(),
-        new RNFirebaseAnalyticsPackage(),
-        new RNFirebaseAuthPackage(),
-        new RNFirebaseRemoteConfigPackage(),
-        new RNFirebaseCrashlyticsPackage(),
-        new RNFirebaseDatabasePackage(),
-        new RNFirebaseFirestorePackage(),
-        new RNFirebaseFunctionsPackage(),
-        new RNFirebaseInstanceIdPackage(),
-        new RNFirebaseLinksPackage(),
-        new RNFirebaseMessagingPackage(),
-        new RNFirebaseNotificationsPackage(),
-        new RNFirebasePerformancePackage(),
-        new RNFirebaseStoragePackage()
-      );
+        @SuppressWarnings("UnnecessaryLocalVariable")
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+            // add/remove these packages as appropriate
+            packages.add(new RNFirebaseAdMobPackage());
+            packages.add(new RNFirebaseAnalyticsPackage());
+            packages.add(new RNFirebaseAuthPackage());
+            packages.add(new RNFirebaseRemoteConfigPackage());
+            packages.add(new RNFirebaseCrashlyticsPackage());
+            packages.add(new RNFirebaseDatabasePackage());
+            packages.add(new RNFirebaseFirestorePackage());
+            packages.add(new RNFirebaseFunctionsPackage());
+            packages.add(new RNFirebaseInstanceIdPackage());
+            packages.add(new RNFirebaseLinksPackage());
+            packages.add(new RNFirebaseMessagingPackage());
+            packages.add(new RNFirebaseNotificationsPackage());
+            packages.add(new RNFirebasePerformancePackage());
+            packages.add(new RNFirebaseStoragePackage());
+        return packages;
     }
 
     @Override
